497369d089003bb41b4416ba96cf5073f896b5bf
Nguyen Thang <ngenvan000@gmail.com>
2022-04-02 09:13:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [counter, setCounter] = useState(0)
+
+  const handlerAdd = () =>{
+    setCounter(counter + 1)
+  }
+  console.log('re-render ', counter);
+  return(
+    <div style={{ margin: 50 }}>
+    <h1>{counter}</h1>
+    <button onClick={() => handlerAdd()}>ADD</button>
     </div>
-  );
+  )
 }
 
 export default App;
