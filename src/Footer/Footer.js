951a9528cff5efefd09883304861ccf65f6896c4
Nguyen Thang <ngenvan000@gmail.com>
2022-05-07 11:47:08 +0700

    update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,10 +1,22 @@
-
+import Group from './Group.js'
+import groups from '../store/footer.js'
 
 function Footer() {
   
   return (
   <div>
-    
+  {
+    groups.map( group =>
+      (
+        <div>{group.title}</div>
+        {
+          group.list.map( info =>
+            <a href={info.url}>{info.name}</a>
+          )
+        }
+      )
+    )
+  }
   </div>
   )
 }
