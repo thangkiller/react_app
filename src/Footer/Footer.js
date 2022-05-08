39ee685130691dcb76df3b63201a24673cb3b29e
Nguyen Thang <ngenvan000@gmail.com>
2022-05-08 11:05:12 +0700

    update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,26 +1,27 @@
 import Group from './Group.js'
 import groups from '../store/footer.js'
+import styles from './footer.module.css'
 
 function Footer() {
-  
+
   return (
-  <div>
-  {
-    groups.map( group =>
-      (
-        <div>
+    <div className={styles.cover}>
+    {
+      groups.map( group =>
+        (
+          <div key={group.id}>
           <div>{group.title}</div>
           { group.list.map( info => (
-            <a href={info.name}>{info.name}</a>
+            <a key={info.id} href={info.name}>{info.name}</a>
             )
           )
-          }
+        }
         </div>
-      )
+        )
+        )
+    }
+    </div>
     )
-  }
-  </div>
-  )
 }
 
 export default Footer;
