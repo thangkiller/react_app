d0563e7dc0342bbfff3e4633331f3e6ac4f9b9bb
Nguyen Thang <ngenvan000@gmail.com>
2022-05-09 22:16:59 +0700

    update

--- src/store/footer.js
+++ src/store/footer.js
@@ -1,86 +1,106 @@
 
 
 const groups = [
+{
+	id: 1,
+	title: 'MONOGRAM',
+	list: [
 	{
-		title: 'MONOGRAM',
-		list: [
-			{
-				name: '305 King St. W.',
-			},
-			{
-				name: 'Suite 502',
-			},
-			{
-			  name: 'Kitchener, ON'
-			},
-			{
-			  name: 'Canada'
-			}
-		]
+		id: 1,
+		name: '305 King St. W.',
 	},
 	{
-    title: 'QUICK LINKS',
-    list: [
-      {
-        name: 'Home',
-        url: ''
-      },
-      {
-        name: 'How it Works',
-        url: ''
-      },
-      {
-        name: 'Shop',
-        url: ''
-      },
-      {
-        name: 'Download',
-        url: ''
-      }
-    ]
+		id: 2,
+		name: 'Suite 502',
 	},
 	{
-	  title: 'HELP',
-	  list: [
-	    {
-	      name: 'FAQs',
-	      url: ''
-	    },
-	    {
-	      name: 'Support Center',
-	      url: ''
-	    },
-	    {
-	      name: 'Shipping and Sales',
-	      url: ''
-	    }
-	  ]
+		id: 3,
+		name: 'Kitchener, ON'
 	},
 	{
-	  title: 'INFOMATION',
-	  list: [
-	    {
-	      name: 'About Us',
-	      url: ''
-	    },
-	    {
-	      name: 'Work with us',
-	      url: ''
-	    },
-	    {
-	      name: 'Privacy Policy',
-	      url: ''
-	    },
-	    {
-	      name: 'Terms of Sale',
-	      url: ''
-	    },
-	    {
-	      name: 'Press Kit',
-	      url: ''
-	    }
-	  ]
+		id: 4,
+		name: 'Canada'
 	}
+	]
+},
+{
+	id: 2,
+	title: 'QUICK LINKS',
+	list: [
+	{
+		id: 1,
+		name: 'Home',
+		url: ''
+	},
+	{
+		id: 2,
+		name: 'How it Works',
+		url: ''
+	},
+	{
+		id: 3,
+		name: 'Shop',
+		url: ''
+	},
+	{
+		id: 4,
+		name: 'Download',
+		url: ''
+	}
+	]
+},
+{
+	id: 3,
+	title: 'HELP',
+	list: [
+	{
+		id: 1,
+		name: 'FAQs',
+		url: ''
+	},
+	{
+		id: 2,
+		name: 'Support Center',
+		url: ''
+	},
+	{
+		id: 3,
+		name: 'Shipping and Sales',
+		url: ''
+	}
+	]
+},
+{
+	id: 4,
+	title: 'INFOMATION',
+	list: [
+	{
+		id: 1,
+		name: 'About Us',
+		url: ''
+	},
+	{
+		id: 2,
+		name: 'Work with us',
+		url: ''
+	},
+	{
+		id: 3,
+		name: 'Privacy Policy',
+		url: ''
+	},
+	{
+		id: 4,
+		name: 'Terms of Sale',
+		url: ''
+	},
+	{
+		id: 5,
+		name: 'Press Kit',
+		url: ''
+	}
+	]
+}
 ]
 
 export default groups
--- src/store/header.js
+++ src/store/header.js
@@ -5,64 +5,63 @@ export const left = {
 }
 export const right = {
 	menu: [
-	  {
-	    id: '01',
-	    title:'HOW IT WORKS',
-	    link:'',
-	  },
-	  {
-	    id: '02',
-	    title: 'WORKFLOWS',
-	    link: '',
-	    type: [
-	      {
-	        id: '12',
-	        title: 'PHOTO EDITING',
-	        url: ''
-	      },
-	      {
-	        id: '22',
-	        title: 'VIDEO & FILMMARKING',
-	        url: ''
-	      },
-	      {
-	        id: '32',
-	        title: 'MUSIC & AUDIO',
-	        url: ''
-	      },
-	      {
-	        id: '42',
-	        title: 'VITUAL PRODUCTION',
-	        url: ''
-	      },
-	      {
-	        id: '52',
-	        title: 'EVERYDAY ESSENTIAL',
-	        url: ''
-	      }
-	    ]
-	  },
-	  {
-	    id: '03',
-	    title:'DOWNLOAD',
-	    link:'',
-	  },
-	  {
-	    id: '04',
-	    title:'BLOG',
-	    link:'',
-	  },
-	  {
-	    id: '05',
-	    title:'SUPPORTED',
-	    link:'',
-	  },
-	  {
-	    id: '06',
-	    title:'SHOP',
-	    link:'',
-	  }
+	{
+		id: '01',
+		title:'HOW IT WORKS',
+		link:'',
+	},
+	{
+		id: '02',
+		title: 'WORKFLOWS',
+		link: '',
+		type: [
+		{
+			id: '12',
+			title: 'PHOTO EDITING',
+			url: ''
+		},
+		{
+			id: '22',
+			title: 'VIDEO & FILMMARKING',
+			url: ''
+		},
+		{
+			id: '32',
+			title: 'MUSIC & AUDIO',
+			url: ''
+		},
+		{
+			id: '42',
+			title: 'VITUAL PRODUCTION',
+			url: ''
+		},
+		{
+			id: '52',
+			title: 'EVERYDAY ESSENTIAL',
+			url: ''
+		}
+		]
+	},
+	{
+		id: '03',
+		title:'DOWNLOAD',
+		link:'',
+	},
+	{
+		id: '04',
+		title:'BLOG',
+		link:'',
+	},
+	{
+		id: '05',
+		title:'SUPPORTED',
+		link:'',
+	},
+	{
+		id: '06',
+		title:'SHOP',
+		link:'',
+	}
 	]
 }
 
-	
