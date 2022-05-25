6571df53e09afa0201c7831ef7a6a3ae8e38763b
Nguyen Thang <ngenvan000@gmail.com>
2022-05-25 22:43:43 +0700

    update

--- src/Header/Pocket.js
+++ src/Header/Pocket.js
@@ -5,31 +5,38 @@ import Basket from "../store/icon/Basket.js";
 import Arrow from "../store/icon/Arrow.js";
 
 const money = 0;
+const isPresent = 2;
+const isDisappear = 1;
+const isHide = 0;
 
 function Pocket() {
-	const [stack, setStack] = useState(true);
+	const [stack, setStack] = useState(isHide);
 
 	return (
-		<button className={styles.pocket} onClick={() => setStack(false)}>
+		<button className={styles.pocket} onClick={() => setStack(isPresent)}>
 			<Basket />
 			<div
 				onClick={(e) => {
-					setStack(true);
+					setStack(isDisappear);
 					e.stopPropagation();
 				}}
 				className={clsx(styles.cover, {
-					[styles.disappear]: stack,
+					[styles.coverPresent]: stack === isPresent,
+					[styles.hide]: stack === isHide,
+					[styles.coverDisappear]: stack === isDisappear,
 				})}
 			></div>
 			<div
 				className={clsx(styles.bag, {
-					[styles.disappear]: stack,
+					[styles.bagPresent]: stack === isPresent,
+					[styles.hide]: stack === isHide,
+					[styles.bagDisappear]: stack === isDisappear,
 				})}
 			>
 				<div className={styles.header}>
 					<div
 						onClick={(e) => {
-							setStack(true);
+							setStack(isDisappear);
 							e.stopPropagation();
 						}}
 					>
