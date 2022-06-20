7a9d36a2489c31f28ec48c96806d4eb478e4c403
Nguyen Thang <ngenvan000@gmail.com>
2022-06-20 23:07:17 +0700

    update

--- src/Body/Poster.js
+++ src/Body/Poster.js
@@ -12,7 +12,20 @@ function Poster() {
 				</h1>
 				<p>Discover the perfect console for yours.</p>
 			</div>
-			<FontAwesomeIcon icon={["fa-light", "fa-angle-down"]} />
+			<a>
+				<svg
+					class="downward-indicator-module--downIcon--Hlhtj animations-module--slideUp--Ztp7W animations-module--slideUpStart--9cuO8"
+					width="1.5em"
+					height="1.5em"
+					fill="currentColor"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+					></path>
+				</svg>
+			</a>
 		</div>
 	);
 }
