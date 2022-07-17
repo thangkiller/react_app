838ac4e11fbb0b2ebbb5faaa13f91ebe85ef8d3a
Nguyen Thang <ngenvan000@gmail.com>
2022-07-17 10:53:06 +0700

    add animation, set height to products

--- src/Body/lay_out/Product.js
+++ src/Body/lay_out/Product.js
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import styles from "./Product.module.scss";
 
 function Product({ name, photo_url, urlhover, price, is_stock }) {
-	const [hover, setHover] = useState(false);
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.image}>
-				<img
-					src={hover ? urlhover : photo_url}
-					onMouseOver={() => setHover(true)}
-					onMouseOut={() => setHover(false)}
-				/>
+			<div className={styles.images}>
+				<img className={styles.img_main} src={photo_url} />
+				<img className={styles.img_hover} src={urlhover} />
 				{is_stock && <div>in stock</div>}
 			</div>
 			<div className={styles.content}>
